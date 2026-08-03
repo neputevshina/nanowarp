74bbc09186fa1fab57b91aa6d7249debe1187c45
neputevshina <51914743+neputevshina@users.noreply.github.com>
2026-08-03 04:24:22 +0400

    BROKEN: wav parser finished

--- dspio/wav/reader.go
+++ dspio/wav/reader.go
@@ -2,88 +2,35 @@ package wav
 
 import (
 	"encoding/binary"
-	"errors"
 	"io"
-	"unsafe"
 )
-
-var NotAWav = errors.New(`not a WAV file`)
-var Malformed = errors.New(`malformed WAV file`)
-var UnknownFormat = errors.New(`file is a WAV, but not in PCM or IEEE float format`)
-
-type Format int
-
-const (
-	FormatPCM        Format = 0x0001
-	FormatFloat             = 0x0003
-	FormatALaw              = 0x0006
-	FormatMuLaw             = 0x0007
-	FormatExtensible        = 0xFFFE
-)
-
-type Properties struct {
-	Samples    int    // Amount of multichannel samples in file
-	Bytes      int    // Length of file in bytes per its RIFF header
-	Samplerate int    // Samples per second
-	Nch        int    // Number of channels
-	Format     Format // Format of samples
-}
-
-type fmtchunk struct {
-	WFormatTag          uint16
-	NChannels           uint16
-	NSamplesPerSec      uint32
-	NAvgBytesPerSec     uint32
-	NBlockAlign         uint16
-	WBitsPerSample      uint16
-	CbSize              uint16
-	WValidBitsPerSample uint16
-	DwChannelMask       uint32
-	SubFormat           [16]byte
-}
 
 type Reader struct {
-	dataseek   int64 // Start of sample data in bytes
-	riff, data riffHeader
+	data Cue // Start of sample data in bytes
+	riff riffHeader
 	fmtchunk
 
-	// A list of all second-level RIFF headers identified in a file, except
-	// `WAVE`, `fmt ` and `data`.
+	// A list of all second-level RIFF headers identified in a file,
+	// except `WAVE` and `fmt `.
 	Headermap []Cue
 }
 
 func (r *Reader) Properties() Properties {
 	return Properties{
-		Samples:    int(r.data.cksize) / int(r.NBlockAlign),
+		Samples:    int(r.data.Size) / int(r.NBlockAlign),
 		Bytes:      int(r.riff.cksize) + 8, // Count RIFFxxxx 8-byte header too
 		Samplerate: int(r.fmtchunk.NSamplesPerSec),
 	}
 }
 
-// Cue is a cue point of a data found from its RIFF section in a file
-// with byte index at which it was found.
-//
-// It points to the start of data.
-type Cue struct {
-	FourCC [4]byte
-	Seek   int64
-	Size   uint32
-}
-
-type riffHeader struct {
-	fourcc [4]byte
-	cksize uint32
-}
-
 func NewReader(rs io.ReadSeeker) (*Reader, error) {
 	r := Reader{}
 
-	var RIFF riffHeader
-	err := binary.Read(rs, binary.LittleEndian, RIFF)
+	err := binary.Read(rs, binary.LittleEndian, r.riff)
 	if err != nil {
 		return nil, err
 	}
-	if string(RIFF.fourcc[:]) != `RIFF` {
+	if string(r.riff.fourcc[:]) != `RIFF` {
 		return nil, NotAWav
 	}
 
@@ -104,12 +51,9 @@ func NewReader(rs io.ReadSeeker) (*Reader, error) {
 	if string(fmt.fourcc[:]) != `fmt ` {
 		return nil, Malformed
 	}
-	if !(fmt.cksize == 40 || fmt.cksize == 16 || fmt.cksize == 18) {
-		return nil, Malformed
-	}
 
-	// Get seek before fmt chunk
-	r.dataseek, err = rs.Seek(0, io.SeekCurrent)
+	// Get seek before fmt chunk.
+	pre, err := rs.Seek(0, io.SeekCurrent)
 	if err != nil {
 		return nil, err
 	}
@@ -117,13 +61,30 @@ func NewReader(rs io.ReadSeeker) (*Reader, error) {
 	if err != nil {
 		return nil, err
 	}
-	// Limit fmt chunk data to its real size.
-	const fmtchunklen = unsafe.Sizeof(r.fmtchunk)
-	clear((*[fmtchunklen]byte)(unsafe.Pointer(&r.fmtchunk))[:fmtchunklen-uintptr(fmt.cksize)])
+	// Cut unpopulated data.
+	if fmt.cksize == 16 || fmt.cksize == 18 {
+		r.fmtchunk.CbSize = 0
+		r.fmtchunk.WValidBitsPerSample = 0
+		r.fmtchunk.DwChannelMask = 0
+		r.fmtchunk.SubFormat = [16]byte{}
+	} else if fmt.cksize != 40 {
+		// Only allowed sizes for fmt chunk are 16, 18 and 40 bytes.
+		return nil, Malformed
+	}
+
 	// Skip fmt chunk correctly.
-	r.dataseek, err = rs.Seek(r.dataseek+int64(fmt.cksize), io.SeekStart)
+	_, err = rs.Seek(pre+int64(fmt.cksize), io.SeekStart)
 	if err != nil {
 		return nil, err
+	}
+	wavfmt := Format(r.fmtchunk.WFormatTag)
+	if !(wavfmt == FormatPCM || wavfmt == FormatFloat) {
+		return nil, UnsupportedFormat
+	}
+	if wavfmt == FormatFloat {
+		if !(r.WBitsPerSample == 32 || r.WBitsPerSample == 64) {
+			return nil, Malformed
+		}
 	}
 
 	for {
@@ -135,11 +96,35 @@ func NewReader(rs io.ReadSeeker) (*Reader, error) {
 			}
 			return nil, err
 		}
-		if string(fmt.fourcc[:]) == `data` {
-
+		seek, err := rs.Seek(0, io.SeekCurrent)
+		if err != nil {
+			return nil, err
 		}
-		break
+
+		q := Cue{
+			Seek:   seek,
+			FourCC: data.fourcc,
+			Size:   data.cksize,
+		}
+		r.Headermap = append(r.Headermap, q)
+		if string(data.fourcc[:]) == `data` {
+			r.data = q
+		}
+
+		_, err = rs.Seek(int64(data.cksize), io.SeekCurrent)
+		if err != nil {
+			if r.data.Seek != 0 && err == io.EOF {
+				// Malformed, but workable.
+				break
+			} else {
+				return nil, err
+			}
+		}
 	}
 
+	_, err = rs.Seek(r.data.Seek, io.SeekStart)
+	if err != nil {
+		return nil, err
+	}
 	return &r, nil
 }
---
+++ dspio/wav/wav.go
@@ -0,0 +1,52 @@
+package wav
+
+import "errors"
+
+var NotAWav = errors.New(`not a WAV file`)
+var Malformed = errors.New(`malformed WAV file`)
+var UnsupportedFormat = errors.New(`file is a WAV, but not in linear PCM or IEEE float format`)
+
+type Format int
+
+const (
+	FormatPCM        Format = 0x0001
+	FormatFloat             = 0x0003
+	FormatALaw              = 0x0006
+	FormatMuLaw             = 0x0007
+	FormatExtensible        = 0xFFFE
+)
+
+type Properties struct {
+	Samples    int    // Amount of multichannel samples in file
+	Bytes      int    // Length of file in bytes per its RIFF header
+	Samplerate int    // Samples per second
+	Nch        int    // Number of channels
+	Format     Format // Format of samples
+}
+
+type fmtchunk struct {
+	WFormatTag          uint16
+	NChannels           uint16
+	NSamplesPerSec      uint32
+	NAvgBytesPerSec     uint32
+	NBlockAlign         uint16
+	WBitsPerSample      uint16
+	CbSize              uint16
+	WValidBitsPerSample uint16
+	DwChannelMask       uint32
+	SubFormat           [16]byte
+}
+
+// Cue is a pointer to a RIFF section in a file.
+//
+// Seek points to the start of data, start of the section itself is at Seek - 8.
+type Cue struct {
+	Seek   int64
+	FourCC [4]byte
+	Size   uint32
+}
+
+type riffHeader struct {
+	fourcc [4]byte
+	cksize uint32
+}
