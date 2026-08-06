a6e899f0fc737437f640ddb9f852d14522c2a537
neputevshina <51914743+neputevshina@users.noreply.github.com>
2026-08-06 13:48:15 +0400

    disclamer in dspio doc comment

--- dspio/doc.go
+++ dspio/doc.go
@@ -1,5 +1,8 @@
 // Experimental package dspio provides primitives and interfaces for basic and granular digital
 // signals I/O, including those for granular and overlap-add-based processing.
+//
+// Please, do not use this package in any contexts not related to Nanowarp or dspio/wavio.
+// The API is not standardized and it is not ready for real-time signal processing.
 package dspio
 
 // NOTES
