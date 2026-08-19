296a5744aafc651dd4b4e7ef420f25972e62d426
neputevshina <51914743+neputevshina@users.noreply.github.com>
2026-08-19 23:52:59 +0400

    len-1, not len in lgd

--- warper.go
+++ warper.go
@@ -493,12 +493,15 @@ func (n *warper) pghiintegrate(arrows [][2]int, Fadv, Tadv, Ph, Past []float64) 
 // osamp is the oversampling ratio and related to shift of the effective window
 // relative to the center of full oversampled window.
 // Inspired by https://ltfat.org/notes/ltfatnote042.pdf.
+//
+// May be replaced with finite bin difference, see Flandrin and Auger, eq. 5.17
+// https://hal.science/hal-00414583/document
 func fadv(x, xt []complex128, stretch, osamp float64, w int) float64 {
 	if cmplx.Abs(x[w]) == 0 {
 		return 0
 	}
-	// NOTE Try len(x)-1 instead. Sounds worse on my $4 speakers.
-	return -real(xt[w]/x[w])/float64(len(x))*math.Pi*stretch - math.Pi/osamp
+	// len(x)-1 is nfft/2.
+	return -real(xt[w]/x[w])/float64(len(x)-1)*math.Pi*stretch - math.Pi/osamp
 }
 
 // tadv calculates the partial derivative of the phase with respect
