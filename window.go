54cb68a7f838a1fc9564dfc5f559f431a54b5d6e
neputevshina <51914743+neputevshina@users.noreply.github.com>
2026-09-22 14:53:22 +0400

    revert anything related to len(window)

--- window.go
+++ window.go
@@ -32,7 +32,7 @@ func hann(out []float64) {
 // https://www.osti.gov/servlets/purl/1365510
 func blackmanHarrisClassic(out []float64) {
 	for i := range out {
-		x := float64(i) / float64(len(out))
+		x := float64(i) / float64(len(out)-1)
 		out[i] = .4243801 - .4973406*math.Cos(2*math.Pi*x) + .0782793*math.Cos(4*math.Pi*x)
 	}
 }
