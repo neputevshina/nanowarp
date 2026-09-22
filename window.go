591c19dfda9fa0221c2d53dbf7c541778912a172
neputevshina <51914743+neputevshina@users.noreply.github.com>
2026-09-22 14:41:16 +0400

    last sample in window is zero

--- window.go
+++ window.go
@@ -31,7 +31,7 @@ func hann(out []float64) {
 // Sandia National Laboratories (SNL-NM), Albuquerque, NM (United States), 2017.
 // https://www.osti.gov/servlets/purl/1365510
 func blackmanHarrisClassic(out []float64) {
-	for i := range out {
+	for i := range out[:len(out)-1] {
 		x := float64(i) / float64(len(out)-1)
 		out[i] = .4243801 - .4973406*math.Cos(2*math.Pi*x) + .0782793*math.Cos(4*math.Pi*x)
 	}
